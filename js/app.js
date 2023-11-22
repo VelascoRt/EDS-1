65e6033bf2ad0006b3b5093a35e6693fdb90c6eb
VelascoRt <velascogmac@gmail.com>
2023-11-22 02:08:58 -0600

    EDS FINAL

--- js/app.js
+++ js/app.js
@@ -14,18 +14,100 @@ hiddenElements.forEach((el) => observer.observe(el));
 var boton = document.getElementById('mostrar');
     var lista = document.getElementById('lista');
     var checks = document.querySelectorAll('#check');
+    var img = document.getElementById("img1");
+    
 
-  boton.addEventListener('click', function () {
+
+boton.addEventListener('click', function () {
     lista.innerHTML = '';
     checks.forEach((e) => {
-      if (e.checked == true) {
-        var elemento = document.createElement('li');
-        elemento.className = 'list-group-item';
-        elemento.innerHTML = e.value;
-        lista.appendChild(elemento);
-      }
+        if (e.checked == true) {
+            var elemento = document.createElement('li');
+            elemento.className = 'list-group-item';
+            elemento.id = e.className;
+            elemento.onclick = image();
+            elemento.innerHTML = e.value;
+            lista.appendChild(elemento);
+        }
     });
-  });
+});
+
+//así con JS normal
+//accediendo directamente a src
+function cambiarImagenJS() {
+    document.getElementById("img1").src = "image2.jpg";
+}
+
+function image() {
+    var j = 0;
+    var img = document.getElementById("img1");
+    var list = document.getElementById('list');
+    var checkboxes = document.querySelectorAll('#check');
+    var srcs = ["img/catalogo1.png", "img/catalogo2.png", "img/catalogo3.png", "img/catalogo4.png", "img/catalogo5.png", "img/catalogo6.png", "img/catalogo7.png", "img/catalogo8.png", "img/catalogo9.png", "img/catalogo10.png", "img/catalogo11.png", "img/catalogo12.png"];
+    var id_src = [];
+
+    if (checkboxes[0].checked) {
+        id_src.push(0);
+    }
+
+    if (checkboxes[1].checked) {
+        id_src.push(1);
+    }
+
+    if (checkboxes[2].checked) {
+        id_src.push(2);
+    }
+
+    if (checkboxes[3].checked) {
+        id_src.push(3);
+    }
+
+    if (checkboxes[4].checked) {
+        id_src.push(4);
+    }
+
+    if (checkboxes[5].checked) {
+        id_src.push(5);
+    }
+
+    if (checkboxes[6].checked) {
+        id_src.push(6);
+    }
+
+    if (checkboxes[7].checked) {
+        id_src.push(7);
+    }
+
+    if (checkboxes[8].checked) {
+        id_src.push(8);
+    }
+
+    if (checkboxes[9].checked) {
+        id_src.push(9);
+    }
+
+    if (checkboxes[10].checked) {
+        id_src.push(10);
+    }
+
+    if (checkboxes[11].checked) {
+        id_src.push(11);
+    }
+    
+    img.addEventListener("click", (event) => {
+        if (j >= id_src.length) {
+            j = 0
+        }
+        if(j>0) {
+            img.src = srcs[id_src[j]];
+            j++;
+        } else {
+            img.src = srcs[id_src[j]];
+            j++;
+        }
+        console.log(j)
+    });
+}
 
 function calculateTotal() {
     var total = 0;
